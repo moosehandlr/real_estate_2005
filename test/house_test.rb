af09f5ec56b618931c65e1943182f31a8ac43d40
Alex Desjardins <ardesjardins91@gmail.com>
2020-05-13 09:48:33 -0600

    Test for house price and address arguments

--- test/house_test.rb
+++ test/house_test.rb
@@ -11,7 +11,12 @@ class HouseTest < Minitest::Test
     assert_instance_of House, house
   end
 
+  def test_house_price_and_rooms
+    house = House.new("$400000", "123 sugar lane")
 
+    assert_equal 400000, house.price
+    assert_equal "123 sugar lane", house.address
+  end
 
 
 end
