5e6599503dae97db1d8274746712a8ccddeda721
Alex Desjardins <ardesjardins91@gmail.com>
2020-05-13 10:02:41 -0600

    Test house can add_room

--- test/house_test.rb
+++ test/house_test.rb
@@ -18,5 +18,24 @@ class HouseTest < Minitest::Test
     assert_equal "123 sugar lane", house.address
   end
 
+  def test_house_rooms_array
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal [], house.rooms
+  end
+
+  def test_house_can_add_rooms
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+
+    house = House.new("$400000", "123 sugar lane")
+
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+
+    assert_equal [room_1, room_2], house.rooms
+  end
+
 
 end
