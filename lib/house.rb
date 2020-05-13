0c937da9f0d9cf061bd7ee2dcdfe8c826ff96095
Alex Desjardins <ardesjardins91@gmail.com>
2020-05-13 09:48:07 -0600

    Add house lib price method.

--- lib/house.rb
+++ lib/house.rb
@@ -1,9 +1,17 @@
 class House
 
+  attr_reader :price,
+              :address
+
   def initialize (price, address)
     @price = price
     @address = address
+    @house_rooms = []
 
+  end
+
+  def price
+    @price.gsub(/\$/,'').to_i
   end
 
 
